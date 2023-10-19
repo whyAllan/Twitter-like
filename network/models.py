aac13752ee0324e6d88c8cc39f49ffa13169fcde
alan shaksper <devalanfreitas@gmail.com>
2023-10-19 19:50:33 -0300

    success

--- network/models.py
+++ network/models.py
@@ -14,7 +14,7 @@ class Profile(models.Model):
         return self.user.username
     # Assert that the user cannot follow themselves
     def is_valid_profile(self):
-        return self.following.filter(username=self.user.username).exists() and not self.followers.filter(username=self.user.username).exists()
+        return not self.following.filter(username=self.user.username).exists() and not self.followers.filter(username=self.user.username).exists()
 
     # Assert that the user cannot follow the same user twice
     def is_following(self, user):
